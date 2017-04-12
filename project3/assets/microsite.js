0b4aeda3b1f0ac8ab50d739586122b64b4764f97
Kelly Haeseker <khaeseker@cca.edu>
2017-04-11 19:11:21 -0700

    too much

---
+++ project3/assets/microsite.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  var el = document.getElementsByClassName('fadefromblack')[0]
+  el.className += " loaded"
+  el = document.getElementsByClassName('title')[0]
+  el.className += " loaded"
+}
