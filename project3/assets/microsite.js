acf7b5b9fa87a6b97a9b82325e275990cacf2c9b
Kelly Haeseker <khaeseker@cca.edu>
2017-04-25 20:27:36 -0700

    updated the site

--- project3/assets/microsite.js
+++ project3/assets/microsite.js
@@ -1,6 +1,4 @@
 window.onload = function() {
-  var el = document.getElementsByClassName('fadefromblack')[0]
-  el.className += " loaded"
   el = document.getElementsByClassName('title')[0]
   el.className += " loaded"
 }
